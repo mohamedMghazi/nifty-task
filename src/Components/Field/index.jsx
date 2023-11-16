5464d87344817ca018822dda748f76edda2b199c
Mohamed Ghazi <62715268+mohamedMghazi@users.noreply.github.com>
2023-11-16 19:57:51 +0200

    Created a reusable field component

---
+++ src/Components/Field/index.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import "./style.scss";
+
+export default function Field(props) {
+    const { title, type, name, placeholder, value, onChange, error, ...rest } = props;
+
+    const updateValue = (event) => {
+        onChange(event.target.value);
+    }
+
+    return (
+        <div className={"field-container"}>
+            <label htmlFor={name}>{title}</label>
+            <input
+                type={type}
+                name={name}
+                id={name}
+                placeholder={placeholder}
+                value={value}
+                onChange={updateValue}
+                data-testid={name}
+                data-error={!!error}
+                {...rest}
+            />
+
+            {!!error && <p className={"field-error"}>{error}</p>}
+        </div>
+    )
+}
+
+Field.propTypes = {
+    title: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    error: PropTypes.string,
+}
