9b9688936039d46268f3516f7697913ccacaca35
Mohamed Ghazi <62715268+mohamedMghazi@users.noreply.github.com>
2023-11-16 19:59:10 +0200

    Added form styles after importing the header

--- src/Pages/Authentication/index.jsx
+++ src/Pages/Authentication/index.jsx
@@ -1,9 +1,11 @@
 import {Outlet} from "react-router-dom";
+import PageHeader from "./Components/PageHeader";
 import "./style.scss";
 
 export default function Authentication() {
     return (
         <main id="authentication-wrapper">
+            <PageHeader />
             <Outlet />
         </main>
     )
