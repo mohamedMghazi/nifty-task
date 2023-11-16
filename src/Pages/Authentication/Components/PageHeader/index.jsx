e580caad622371ea30f253602064eac5ea9cb551
Mohamed Ghazi <62715268+mohamedMghazi@users.noreply.github.com>
2023-11-16 17:51:47 +0200

    Implemented PageHeader component

---
+++ src/Pages/Authentication/Components/PageHeader/index.jsx
@@ -0,0 +1,9 @@
+import "./style.scss";
+
+export default function PageHeader () {
+    return (
+        <header className={"page-header-container"}>
+            <h1>NiftyTask</h1>
+        </header>
+    )
+}
