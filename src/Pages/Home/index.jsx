ff2bc325376a2b96712f961425b78a2bd89c94c8
Mohamed Ghazi <62715268+mohamedMghazi@users.noreply.github.com>
2023-11-17 16:09:45 +0200

    Added home navbar

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,32 @@
+import {useEffect} from "react";
+import CookieManager from "../../Utils/Storage/CookiesManager";
+
+import SolidButton from "../../Components/SolidButton";
+
 import "./style.scss";
 
 export default function Home() {
+    const cookies = new CookieManager();
+
+    const logout = () => {
+        cookies.remove("X-USER-TOKEN");
+        cookies.remove("X-USER-DATA");
+        window.location.reload();
+    }
+
+    useEffect(() => {
+        document.body.style.background = "#f4f6f7"
+    }, []);
+
     return (
         <main id={"home-wrapper"}>
             <nav>
-                Navbar
+                <h1>NiftyTask</h1>
+
+                <SolidButton
+                    title={"Logout"}
+                    onClick={logout}
+                />
             </nav>
 
             <section>
