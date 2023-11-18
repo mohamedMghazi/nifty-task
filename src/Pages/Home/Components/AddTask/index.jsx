8487fc0c2bc3c1b9d6b53260cd9561187636bca7
Mohamed Ghazi <62715268+mohamedMghazi@users.noreply.github.com>
2023-11-18 20:49:51 +0200

    Used thunk to add a task instead of custom implementation

--- src/Pages/Home/Components/AddTask/index.jsx
+++ src/Pages/Home/Components/AddTask/index.jsx
@@ -1,5 +1,7 @@
 import {useState} from "react";
-import API from "Utils/API";
+import {useDispatch, useSelector} from "react-redux";
+
+import {addTask} from "Utils/Redux/Slices/Tasks/actions";
 
 import Field from "Components/Field";
 import SolidButton from "Components/SolidButton";
@@ -7,57 +9,39 @@ import SolidButton from "Components/SolidButton";
 import "./style.scss";
 
 export default function AddTask() {
-    const [taskName, setTaskName] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
+    const dispatch = useDispatch();
+    const { status, error } = useSelector(state => state.tasks);
+    const [internalError, setInternalError] = useState("");
 
-    const addTask = (e) => {
+    const [taskName, setTaskName] = useState("");
+
+    const handleTaskSubmit = (e) => {
         e.preventDefault();
 
         if (taskName.length < 1) {
-            return setError("Please enter a task name");
+            return setInternalError("Please enter a task name");
         }
 
-        setLoading(true);
-        setError("");
-
-        const { apiResponse } = API({ endpoint: 'tasks/create', method: "POST", data: { title: taskName } });
-        apiResponse()
-            .then(({ data, status }) => {
-                if (status === 200) {
-                    // TODO: Re-fetch tasks list
-                    setTaskName("");
-                } else {
-                    setError("Something went wrong, please try again later");
-                }
-            })
-            .catch((e) => {
-                console.log(e);
-                if (e?.response && e?.response?.data) {
-                    const { errors } = e.response.data;
-                    setError(errors);
-                }
-            })
-            .finally(() => {
-                setLoading(false);
-            });
+        dispatch(addTask({ title: taskName }));
+        setTaskName("");
+        setInternalError("");
     }
 
     return (
-        <form id={"add-task-form"} data-testid={"add-task-form"} onSubmit={addTask}>
+        <form id={"add-task-form"} data-testid={"add-task-form"} onSubmit={handleTaskSubmit}>
             <Field
                 type={"text"}
                 name={"task-name"}
                 placeholder={"I plan to do something nifty today!"}
                 value={taskName}
                 onChange={taskName => setTaskName(taskName)}
-                error={error}
+                error={error || internalError}
             />
 
             <SolidButton
                 type={"submit"}
                 title={"Add Task"}
-                loading={loading}
+                loading={status === "loading"}
             />
         </form>
     );
